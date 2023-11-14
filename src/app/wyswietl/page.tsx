3a1e8b03781cb43be592389bcd39ed85e2b28e70
Kamil Kiliasiński <kamilkiliasinski@gmail.com>
2023-11-15 00:48:23 +0100

    add block as a demo

--- src/app/wyswietl/page.tsx
+++ src/app/wyswietl/page.tsx
@@ -2,7 +2,7 @@
 
 import { useData } from "@/lib/store-zustand";
 import useStore from "@/lib/use-store";
-import { createContext, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { DataTableShow } from "./components/data-table-show";
 import { DataAfterCalculation } from "@/types/types";
 import { columnsShow } from "./components/columns";
@@ -12,6 +12,12 @@ import ChartSCI from "./components/chart-sci";
 import ChartBCI from "./components/chart-bci";
 import Legend from "./components/legend";
 import { DataContext } from "./components/context-data";
+import {
+  Card,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 
 const Map = dynamic(() => import("./components/map"), {
   ssr: false,
@@ -37,28 +43,15 @@ export default function ShowResults() {
   });
 
   return (
-    <div className="my-10">
-      {allData && (
-        <DataTableShow
-          rowSelection={rowSelection}
-          setRowSelection={setRowSelection}
-          data={allData}
-          columns={columnsShow}
-        />
-      )}
-      {filteredSelectedData.length === 1 ? (
-        <div className="flex w-full gap-4">
-          <Legend />
-          <div className="flex flex-col w-1/2 grow gap-4">
-            <DataContext.Provider value={filteredSelectedData}>
-              <ChartSCI />
-              <ChartBDI />
-              <ChartBCI />
-              {/* <Map /> */}
-            </DataContext.Provider>
-          </div>
-        </div>
-      ) : null}
+    <div className="w-full h-screen flex justify-center items-center">
+      <Card className="max-w-lg">
+        <CardHeader>
+          <CardDescription className="text-xl">
+            Funkcjonalność strony zostanie odblokowana po przeprowadzeniu
+            pierwszego szkolenia w umówionym terminie
+          </CardDescription>
+        </CardHeader>
+      </Card>
     </div>
   );
 }
